91f0d2b4e39db74d9edb305a7318bbd08f02b87c
Md Nahid Hossain <nahidhossainmd99@gmail.com>
2026-01-30 15:25:07 +0600

    first commit

---
+++ sync_playwright.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as p:
+    
+    browser=p.chromium.launch(headless=False)
+    
+    page=browser.new_page()
+    page.goto("https://www.udemy.com/")
+    
+    print(page.title())
+    
+    browser.close()
+    
+
