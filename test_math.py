a7cc05f5047eeb92b64149aa30a39e62bc4fd4b9
Md Nahid Hossain <nahidhossainmd99@gmail.com>
2026-01-31 13:06:57 +0600

    first commit

--- test_math.py
+++ test_math.py
@@ -1,9 +1,13 @@
+import pytest
+from maths import add, divide
+
 def test_add():
-    assert 2+3==5
-    
-def test_subtraction():
-    assert 6-2==3
-    
-def test_multiplication():
-    assert 2*3==6
-    
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_divide_normal():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
